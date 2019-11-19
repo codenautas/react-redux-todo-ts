a9a5fbf87ddca206705d570863d63f97c46559f4
Emilio Platzer <emilioplatzer@gmail.com>
2019-11-18 23:39:46 -0300

    adaptando el ejemplo a redux-typed-reducer

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,5 +1,7 @@
 import Todo from '../models/Todo'
-import { ActionTypes, Action } from '../actions/todos'
+// import { ActionTypes, Action } from '../actions/todos'
+
+import { createReducer, ActionsFrom } from 'redux-typed-reducer';
 
 // Define our State interface for the current reducer
 export interface State {
@@ -11,41 +13,30 @@ export const initialState: State = {
   todos: [] // We don't have any todos at the start of the app
 }
 
+export const todoReducers = {
+  ADD_TODO: (payload: {todo: Todo}) => (
+    (state: State) =>  (
+      {
+        ...state,
+        todos: [...state.todos, payload.todo] // Add todo to todos array
+      }
+    )
+  ),
+  TOGGLE_TODO: (payload: {todoId: number}) => (
+    (state: State) => (
+      {
+        ...state,
+        todos: state.todos.map(todo => todo.id === payload.todoId ? { ...todo, done: !todo.done } : todo)
+      }
+    )
+  )
+}
+
+type AF = ActionsFrom<State, typeof todoReducers>;
+
 /* 
  * Reducer takes 2 arguments
  * state: The state of the reducer. By default initialState ( if there was no state provided)
  * action: Action to be handled. Since we are in todos reducer, action type is Action defined in our actions/todos file.
  */
-export function reducer(state: State = initialState, action: Action) {
-  switch (action.type) {
-
-    case ActionTypes.ADD_TODO: {
-      /*
-       * We have autocompletion here
-       * Typescript knows the action is type of AddTodoAction thanks to the ActionTypes enum
-       * todo is type of Todo
-       */
-      const todo = action.payload.todo
-
-      return {
-        ...state,
-        todos: [...state.todos, todo] // Add todo to todos array
-      }
-    }
-
-    case ActionTypes.TOGGLE_TODO: {
-      /*
-       * This is the same as 
-       * const todoId = action.payload.todoId
-       */
-      const { todoId } = action.payload
-      return {
-        ...state,
-        todos: state.todos.map(todo => todo.id === todoId ? { ...todo, done: !todo.done } : todo)
-      }
-    }
-
-    default:
-      return state
-  }
-}
+export var reducer = createReducer(todoReducers, initialState);
